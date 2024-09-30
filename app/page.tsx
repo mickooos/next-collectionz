991d1b71a717b561e9269ad649f1b750b5b5fced
unknown <mickoooooos@gmail.com>
2024-10-01 02:11:48 +0700

    change the layout

--- app/page.tsx
+++ app/page.tsx
@@ -6,21 +6,25 @@ export default async function Home() {
   const collections = await getCollections();
 
   return (
-    <div className="min-h-screen p-4 flex flex-col items-center">
-      {/* Header Section */}
-      <div className="w-full flex justify-between items-center bg-white p-4 shadow-md">
-        <h1 className="text-2xl font-bold font-title">Latest Collection</h1>
+    <div className="min-h-screen bg-slate-50 p-6">
+      {/* Header */}
+      <div className="flex justify-between items-center mb-6 max-w-6xl mx-auto">
+        {/* Project Title */}
+        <h1 className="text-[15px] sm:text-lg lg:text-3xl font-bold font-title">
+          Latest Collection
+        </h1>
+        {/* Button on the Right */}
         <Link
           href="/create"
-          className="py-2 px-4 text-xl rounded-lg bg-gradient-to-r text-white
+          className="px-4 py-2 text-[10px] sm:text-md md:text-lg font-bold rounded-lg bg-gradient-to-r text-white
                    from-orange-700 to-emerald-700 hover:from-orange-600 hover:to-blue-700"
         >
           Add Collection
         </Link>
       </div>
 
-      {/* Card Section */}
-      <div className="grid gap-6 mt-4 w-full max-w-6xl p-4 sm:grid-cols-2 lg:grid-cols-3">
+      {/* Card Grid */}
+      <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {collections?.map((item) => (
           <Card key={item.id} data={item} />
         ))}
